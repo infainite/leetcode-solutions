85a5d68cb11c417ff7da7e7239a379de420cb0c3
infainite <infainite@proton.me>
2023-09-22 21:03:38 +0530

    solved daily temperatures

--- daily-temperatures/c++/solution.cpp
+++ daily-temperatures/c++/solution.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
-    void solve(vector<int>& arr, int start, int end, vector<int>& res)
-    {
-        bool found = false;
-
-        if (start == end) return;
-
-        int atStart = arr.at(start);
-        
-        for (int i = start + 1; i < end + 1; i++)
-        {
-            if (arr.at(i) > atStart)
-            {
-                found = true;
-                res.at(start) = i - start;
-                solve(arr, start + 1, end, res);
-                solve(arr, end, arr.size() - 1, res);
-                break;
-            }
-        }
-
-        if (!found)
-            solve(arr, start + 1, end, res);
-    }
-    
     vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<int> t;
         vector<int> res(temperatures.size(), 0);
 
-        solve(temperatures, 0, temperatures.size() - 1, res);
+        for (int i = 0; i < temperatures.size(); i++)
+        {
+          if (t.empty())
+            t.push(i);
+          else if (temperatures[t.top()] < temperatures[i])
+          {
+            res[t.top()] = i - t.top();
+            t.pop();
+            i--;
+          }
+          else
+          {
+            t.push(i);
+          }
+        }
 
         return res;
     }
